429535cde871b0ef6667c8c61a4f1306aa4a9154
diegoOG09 <xdiegoemmax@outlook.com>
2023-10-03 12:18:00 -0600

    Add ProductList view

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <h1>Lista de Productos</h1>
-    <button @click="sortById">Ordenar por ID</button>
-    <button @click="sortByPrice">Ordenar por Precio</button>
+    <button @click="toggleSortOrder">Alternar Orden</button>
+    <p>Da clic en "ID" para ordenar por ID o clic en "Precio" para ordenar por precio</p>
     <table>
       <thead>
         <tr>
-          <th>ID</th>
+          <th @click="sortBy('id')">ID</th>
           <th>Título</th>
-          <th>Precio</th>
+          <th @click="sortBy('price')">Precio</th>
           <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="product in sortedProducts" :key="product.id">
+        <tr v-for="product in sortedProducts2" :key="product.id">
           <td>{{ product.id }}</td>
           <td>{{ product.title }}</td>
           <td>{{ product.price }}</td>
           <td>
-            <button @click="viewDetails(product.id)">Ver más</button>
+            <router-link :to="'/products/' + product.id">Ver más</router-link>
           </td>
         </tr>
       </tbody>
@@ -30,6 +30,8 @@
 import { ref, computed, onMounted } from 'vue'
 
 const products = ref([])
+const sortOrder = ref('asc');
+const sortField = ref('id');
 
 onMounted(async () => {
   try {
@@ -44,6 +46,40 @@ onMounted(async () => {
 
 const sortedProducts = ref([]);
 
+const sortedProducts2 = computed(() => {
+  if (sortOrder.value === 'asc') {
+    // Ordenar de forma ascendente
+    return [...products.value].sort((a, b) =>
+      sortField.value === 'price' ? a.price - b.price : a.id - b.id
+    );
+  } else if (sortOrder.value === 'desc') {
+    // Ordenar de forma descendente
+    return [...products.value].sort((a, b) =>
+      sortField.value === 'price' ? b.price - a.price : b.id - a.id
+    );
+  } else {
+    // No se ordena
+    return [...products.value];
+  }
+});
+
+const sortBy = (field) => {
+  sortField.value = field; // Actualizar el campo de ordenamiento
+  if (sortOrder.value === 'asc') {
+    sortOrder.value = 'desc'; // Cambiar a orden descendente
+  } else {
+    sortOrder.value = 'asc'; // Cambiar a orden ascendente
+  }
+};
+
+const toggleSortOrder = () => {
+  if (sortOrder.value === 'asc') {
+    sortOrder.value = 'desc'; // Cambiar a orden descendente
+  } else {
+    sortOrder.value = 'asc'; // Cambiar a orden ascendente
+  }
+};
+
 const sortById = () => {
   sortedProducts.value = [...products.value].sort((a, b) => a.id - b.id);
 };
@@ -51,10 +87,6 @@ const sortById = () => {
 const sortByPrice = () => {
   sortedProducts.value = [...products.value].sort((a, b) => a.price - b.price);
 };
-
-const viewDetails = (productId) => {
-  // Redirige
-}
 
 
 </script>
